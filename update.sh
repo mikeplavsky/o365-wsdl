ee46c9e460983e4cb2e92588fd4a137a578d0237
Mike Plavsky <Mike.Plavsky@quest.com>
2013-08-14 22:43:59 +0000

    added xmllint formatting

--- update.sh
+++ update.sh
@@ -1,4 +1,6 @@
 curl https://provisioningapi.microsoftonline.com/ProvisioningWebService.svc?wsdl -o ProvisioningWebService.xml
+xmllint --format ProvisioningWebService.xml -o ProvisioningWebService.xml
+
 git add .
 git commit -m "changes"
 git push
